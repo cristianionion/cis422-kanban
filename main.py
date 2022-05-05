a24ff88654357e8f603d450353fd83c69fb37162
Brandon Dodd <legendarysandbird@gmail.com>
2022-05-04 23:39:02 -0700

    Create example in main.py
    Even though I haven't fleshed out the design yet I created an example
    using what currently works

--- main.py
+++ main.py
@@ -8,4 +8,32 @@ from Board import *
 root = Tk()
 root.title("Kanban")
 
+board = Board("Sprint 1")
+tk_board = board.gen(root)
+tk_board.grid(column=0, row=0)
+
+b1 = Bucket("Not Started")
+tk_b1 = b1.gen(tk_board)
+tk_b1.grid(column=0, row=0, sticky="n")
+
+c1 = Card("Card 1", "Description 1")
+tk_c1 = c1.gen(tk_b1)
+tk_c1.grid(column=0, row=1)
+
+c2 = Card("Card 2", "Description 2")
+tk_c2 = c2.gen(tk_b1)
+tk_c2.grid(column=0, row=2)
+
+b2 = Bucket("In Progress")
+tk_b2 = b2.gen(tk_board)
+tk_b2.grid(column=1, row=0, sticky="n")
+
+c3 = Card("Card 3", "Description 3")
+tk_c3 = c3.gen(tk_b2)
+tk_c3.grid(column=0, row=1)
+
+b3 = Bucket("Completed")
+tk_b3 = b3.gen(tk_board)
+tk_b3.grid(column=2, row=0, sticky="n")
+
 root.mainloop()
