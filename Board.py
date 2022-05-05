3e2ebd60f80caa5c128655021daecad879ac0d77
Brandon Dodd <legendarysandbird@gmail.com>
2022-05-05 09:30:58 -0700

    Redesign the way that tkinter generates the board
    There is now a gen method for every class that recursively calls the gen
    method for all of its children then attaches it to some parent object.
    This is more elegant than before since you only need to call the gen
    method on one object and then attach it.

--- Board.py
+++ Board.py
@@ -4,13 +4,22 @@ from tkinter import ttk
 from typing import List
 
 class Board:
-    def __init__(self, title: str):
+    def __init__(self, title: str, buckets: List["bucket"]):
         self.title = title
+        self.buckets = buckets
 
     def gen(self, root):
         boardframe = ttk.Frame(root, padding="3 3 12 12")
 
         title = ttk.Label(boardframe, text=self.title)
         title.grid(column=0, row=0, sticky="nsew")
+
+        bucketholder = ttk.Frame(boardframe, padding="3 3 12 12")
+
+        for i in range(len(self.buckets)):
+            bucketframe = self.buckets[i].gen(bucketholder)
+            bucketframe.grid(row=0, column=i, sticky="n")
+
+        bucketholder.grid(column=0, row=1)
 
         return boardframe
--- Bucket.py
+++ Bucket.py
@@ -4,13 +4,22 @@ from tkinter import ttk
 from typing import List
 
 class Bucket:
-    def __init__(self, title: str):
+    def __init__(self, title: str, cards: List["Card"]):
         self.title = title
+        self.cards = cards
 
     def gen(self, board):
         bucketframe = ttk.Frame(board, padding="3 3 12 12")
 
         title = ttk.Label(bucketframe, text=self.title)
         title.grid(column=0, row=0, sticky="nsew")
+
+        cardholder = ttk.Frame(bucketframe, padding="3 3 12 12")
+
+        for i in range(len(self.cards)):
+            cardframe = self.cards[i].gen(cardholder)
+            cardframe.grid(column=0, row=i)
+
+        cardholder.grid(column=0, row=1)
 
         return bucketframe
