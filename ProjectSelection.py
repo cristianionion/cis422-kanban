685ccb0e5fc240bcdff5218ec4eb9c4adb17cf2b
Brandon Dodd <legendarysandbird@gmail.com>
2022-05-17 14:19:18 -0700

    Add a way to select projects from the main menu
    You can now create new Boards and then select them. The Boards do not
    save but they are created with the correct name.

--- ProjectSelection.py
+++ ProjectSelection.py
@@ -1,10 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
+from Board import *
+from Bucket import *
+
 class ProjectSelection:
     def __init__(self, root):
         self.root = root
         self.boards = []
+        self.name_field = StringVar()
 
     def gen(self):
         mainframe = ttk.Frame(self.root)
@@ -12,19 +16,42 @@ class ProjectSelection:
         login_btn = ttk.Button(mainframe, text="Login")
         login_btn.grid(column=0, row=0)
 
+        name_entry = ttk.Entry(mainframe, text="Name", textvariable=self.name_field)
+        name_entry.grid(column=0, row=1)
+
         add_board_btn = ttk.Button(mainframe, text="Add Board", command=self.add_board)
-        add_board_btn.grid(column=1, row=0)
+        add_board_btn.grid(column=1, row=1)
 
         for i in range(len(self.boards)):
-            b = ttk.Button(mainframe, text=self.boards[i])
-            b.grid(column=0, row=i+1) 
+            b = ttk.Button(mainframe, text=self.boards[i], command=lambda: self.enter_board(b))
+            b.grid(column=0, row=i+2) 
 
         return mainframe
 
     def add_board(self):
-        self.boards.append("Name")
+        name = self.name_field.get()
 
+        if len(name) > 0:
+            self.name_field.set("")
+            self.boards.append(name)
+
+            for widget in self.root.grid_slaves():
+                widget.grid_forget()
+
+            self.gen().grid(column=0, row=0)
+
+    def enter_board(self, event):
         for widget in self.root.grid_slaves():
             widget.grid_forget()
 
-        self.gen().grid(column=0, row=0)
+        buckets = [Bucket("Not Started", []),
+                   Bucket("In Progress", []),
+                   Bucket("Completed", [])
+                   ]
+
+        b = Board(self.root, event["text"], buckets)
+
+        for bucket in buckets:
+            bucket.change_parent_to(b)
+
+        b.gen().grid(row=0, column=0)
--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 
-from Card import *
-from Bucket import *
-from Board import *
+from ProjectSelection import *
 
 root = Tk()
 root.title("Kanban")
 
-b1 = Bucket("Not Started", [])
-b2 = Bucket("In Progress", [])
-b3 = Bucket("Completed", [])
-
-board = Board(root, "Sprint 1", [b1, b2, b3])
-boardholder = board.gen()
-boardholder.grid(column=0, row=0)
-
-for b in b1,b2,b3:
-    b.change_parent_to(board)
+ProjectSelection(root).gen().grid(column=0, row=0)
 
 root.mainloop()
